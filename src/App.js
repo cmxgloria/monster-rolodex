f194b238fc89ff7e04da29945147755f058869aa
Gloria Chen <cmxgloria@gmail.com>
2020-04-11 10:54:45 +1000

    updated initial

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      string: "hello gloria",
+    };
+  }
+  render() {
+    return (
+      <div>
+        <p>{this.state.string}</p>
+        <button
+          onClick={() => {
+            this.setState({ string: "hello matthew" });
+          }}
+        ></button>
+      </div>
+    );
+  }
 }
 
 export default App;
