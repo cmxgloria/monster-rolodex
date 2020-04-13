5814d922605ead462fd99c13c8e6ccb49d29bfcc
Gloria Chen <cmxgloria@gmail.com>
2020-04-13 22:53:28 +1000

    Updates

---
+++ precache-manifest.35f39e806b18c5379c22cb6f2847ece8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5173748bffdec87a280dbb8ad95e78a",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "8069a38dd7304e4bfb28",
+    "url": "/monster-rolodex/static/css/main.84c75008.chunk.css"
+  },
+  {
+    "revision": "9d84f5b306774dfc9f1a",
+    "url": "/monster-rolodex/static/js/2.91e906d3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-rolodex/static/js/2.91e906d3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8069a38dd7304e4bfb28",
+    "url": "/monster-rolodex/static/js/main.72ef8e2c.chunk.js"
+  },
+  {
+    "revision": "238059c1b43ed123953f",
+    "url": "/monster-rolodex/static/js/runtime-main.14143c00.js"
+  }
+]);
