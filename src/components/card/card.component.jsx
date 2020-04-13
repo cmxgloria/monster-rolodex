c36be86ad81498eaa3298fae658db8745214014e
Gloria Chen <cmxgloria@gmail.com>
2020-04-14 00:07:37 +1000

    updated deploy

--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -5,7 +5,7 @@ export const Card = (props) => (
   <div className="card-container">
     <img
       // set=set2 is parameter , it shows on api web
-      src={`https://robohash.org/${props.monster.id}?set=set2&size=180x140`}
+      src={`https://robohash.org/${props.monster.id}?set=set2&size=180x180`}
       alt="monster"
     />
     <h2>{props.monster.name}</h2>
